e44af2576cf4ec360e0ef6271826cb995fbdef83
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2025-02-01 10:55:32 +0100

    extra

--- src/main/java/dev/pawel/extra/SortedDoublyLinkedList.java
+++ src/main/java/dev/pawel/extra/SortedDoublyLinkedList.java
@@ -4,6 +4,7 @@ public class SortedDoublyLinkedList {
     private Node head;
     private Node tail;
     private int size;
+    private boolean isNotReverseAlphabet;
 
     private SortedDoublyLinkedList(Node head, Node tail, int size) {
         this.head = head;
@@ -15,33 +16,70 @@ public class SortedDoublyLinkedList {
         this(null, null, 0);
     }
 
+    public Node get(int index) {
+        checkIndex(index);
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+
     public void add(Student student) {
         Node node = new Node(student);
         if (head == null) {
             head = node;
             tail = node;
-        } else if (student.compareTo(head.getStudent()) <= 0) {
-            node.setNext(head);
-            head.setPrevious(node);
-            head = node;
-        } else if (student.compareTo(tail.getStudent()) > 0) {
-            node.setPrevious(tail);
-            tail.setNext(node);
-            tail = node;
         } else {
-            Node temp = head;
-            while (temp.getNext() != null) {
-                if (temp.getStudent().compareTo(student) > 0) {
-                    break;
+            if (isNotReverseAlphabet) {
+                if (student.compareTo(head.getStudent()) >= 0) {
+                    node.setNext(head);
+                    head.setPrevious(node);
+                    head = node;
+                } else if (student.compareTo(tail.getStudent()) < 0) {
+                    node.setPrevious(tail);
+                    tail.setNext(node);
+                    tail = node;
+                } else {
+                    Node temp = head;
+                    while (temp.getNext() != null) {
+                        if (temp.getStudent().compareTo(student) < 0) {
+                            break;
+                        }
+                        temp = temp.getNext();
+                    }
+                    node.setNext(temp);
+                    node.setPrevious(temp.getPrevious());
+                    if (temp.getPrevious() != null) {
+                        temp.getPrevious().setNext(node);
+                    }
+                    temp.setPrevious(node);
                 }
-                temp = temp.getNext();
+            } else {
+                if (student.compareTo(head.getStudent()) <= 0) {
+                    node.setNext(head);
+                    head.setPrevious(node);
+                    head = node;
+                } else if (student.compareTo(tail.getStudent()) > 0) {
+                    node.setPrevious(tail);
+                    tail.setNext(node);
+                    tail = node;
+                } else {
+                    Node temp = head;
+                    while (temp.getNext() != null) {
+                        if (temp.getStudent().compareTo(student) > 0) {
+                            break;
+                        }
+                        temp = temp.getNext();
+                    }
+                    node.setNext(temp);
+                    node.setPrevious(temp.getPrevious());
+                    if (temp.getPrevious() != null) {
+                        temp.getPrevious().setNext(node);
+                    }
+                    temp.setPrevious(node);
+                }
             }
-            node.setNext(temp);
-            node.setPrevious(temp.getPrevious());
-            if (temp.getPrevious() != null) {
-                temp.getPrevious().setNext(node);
-            }
-            temp.setPrevious(node);
         }
         size++;
     }
@@ -75,14 +113,13 @@ public class SortedDoublyLinkedList {
     }
 
     public void removeAt(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for list of size " + size);
-        }
-        if(index == 0) {
+        checkIfListIsEmpty();
+        checkIndex(index);
+        if (index == 0) {
             removeFirst();
             return;
         }
-        if (index == size -1) {
+        if (index == size - 1) {
             removeLast();
             return;
         }
@@ -93,6 +130,12 @@ public class SortedDoublyLinkedList {
         temp.getPrevious().setNext(temp.getNext());
         temp.getNext().setPrevious(temp.getPrevious());
         size--;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for list of size " + size);
+        }
     }
 
     public void supplyTo(Consumer consumer) {
@@ -108,6 +151,7 @@ public class SortedDoublyLinkedList {
             return;
         tail = head;
         head = reverseImp(head);
+        isNotReverseAlphabet = !isNotReverseAlphabet;
     }
 
     private Node reverseImp(Node node) {
---
+++ src/test/java/dev/pawel/extra/SortedDoublyLinkedListTest.java
@@ -0,0 +1,366 @@
+package dev.pawel.extra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SortedDoublyLinkedListTest {
+
+    @Test
+    void shouldAddStudentToEmptyList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        //When
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //Then
+        assertEquals(1, sortedDoublyLinkedList.size());
+        assertEquals("Pawel", sortedDoublyLinkedList.get(0).getStudent().name());
+        assertEquals("Kowalski", sortedDoublyLinkedList.get(0).getStudent().surname());
+        assertEquals(2342, sortedDoublyLinkedList.get(0).getStudent().s());
+    }
+
+    @Test
+    void shouldAddTwoStudentToListAndSortThem() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        //When
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //Then
+        assertEquals(2, sortedDoublyLinkedList.size());
+        assertEquals("Pawel", sortedDoublyLinkedList.get(0).getStudent().name());
+        assertEquals("Kowalski", sortedDoublyLinkedList.get(0).getStudent().surname());
+        assertEquals(2342, sortedDoublyLinkedList.get(0).getStudent().s());
+        assertEquals("Damian", sortedDoublyLinkedList.get(1).getStudent().name());
+        assertEquals("Nowak", sortedDoublyLinkedList.get(1).getStudent().surname());
+        assertEquals(6142, sortedDoublyLinkedList.get(1).getStudent().s());
+    }
+    @Test
+    void shouldAddSixStudentsAndSortThem_whenOrderIsAToZ() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        //When
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(romanSmith);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(patrykSmith);
+        //Then
+        assertEquals(6, sortedDoublyLinkedList.size());
+        assertEquals("Robert", sortedDoublyLinkedList.get(0).getStudent().name());
+        assertEquals("Kowalczyk", sortedDoublyLinkedList.get(0).getStudent().surname());
+        assertEquals(6742, sortedDoublyLinkedList.get(0).getStudent().s());
+        assertEquals("Pawel", sortedDoublyLinkedList.get(1).getStudent().name());
+        assertEquals("Kowalski", sortedDoublyLinkedList.get(1).getStudent().surname());
+        assertEquals(2342, sortedDoublyLinkedList.get(1).getStudent().s());
+        assertEquals("Rafal", sortedDoublyLinkedList.get(2).getStudent().name());
+        assertEquals("Nawrocki", sortedDoublyLinkedList.get(2).getStudent().surname());
+        assertEquals(2543, sortedDoublyLinkedList.get(2).getStudent().s());
+        assertEquals("Damian", sortedDoublyLinkedList.get(3).getStudent().name());
+        assertEquals("Nowak", sortedDoublyLinkedList.get(3).getStudent().surname());
+        assertEquals(6142, sortedDoublyLinkedList.get(3).getStudent().s());
+        assertEquals("Patryk", sortedDoublyLinkedList.get(4).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(4).getStudent().surname());
+        assertEquals(1411, sortedDoublyLinkedList.get(4).getStudent().s());
+        assertEquals("Roman", sortedDoublyLinkedList.get(5).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(5).getStudent().surname());
+        assertEquals(3312, sortedDoublyLinkedList.get(5).getStudent().s());
+    }
+    @Test
+    void shouldReverseList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(romanSmith);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(patrykSmith);
+        //When
+        sortedDoublyLinkedList.reverse();
+        //Then
+        assertEquals(6, sortedDoublyLinkedList.size());
+        assertEquals("Roman", sortedDoublyLinkedList.get(0).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(0).getStudent().surname());
+        assertEquals(3312, sortedDoublyLinkedList.get(0).getStudent().s());
+        assertEquals("Patryk", sortedDoublyLinkedList.get(1).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(1).getStudent().surname());
+        assertEquals(1411, sortedDoublyLinkedList.get(1).getStudent().s());
+        assertEquals("Damian", sortedDoublyLinkedList.get(2).getStudent().name());
+        assertEquals("Nowak", sortedDoublyLinkedList.get(2).getStudent().surname());
+        assertEquals(6142, sortedDoublyLinkedList.get(2).getStudent().s());
+        assertEquals("Rafal", sortedDoublyLinkedList.get(3).getStudent().name());
+        assertEquals("Nawrocki", sortedDoublyLinkedList.get(3).getStudent().surname());
+        assertEquals(2543, sortedDoublyLinkedList.get(3).getStudent().s());
+        assertEquals("Pawel", sortedDoublyLinkedList.get(4).getStudent().name());
+        assertEquals("Kowalski", sortedDoublyLinkedList.get(4).getStudent().surname());
+        assertEquals(2342, sortedDoublyLinkedList.get(4).getStudent().s());
+        assertEquals("Robert", sortedDoublyLinkedList.get(5).getStudent().name());
+        assertEquals("Kowalczyk", sortedDoublyLinkedList.get(5).getStudent().surname());
+        assertEquals(6742, sortedDoublyLinkedList.get(5).getStudent().s());
+    }
+    @Test
+    void shouldRemoveLastStudent() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(romanSmith);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        //When
+        sortedDoublyLinkedList.removeLast();
+        //Then
+        assertEquals(3, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldRemoveFirstStudent() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(romanSmith);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        //When
+        sortedDoublyLinkedList.removeFirst();
+        //Then
+        assertEquals(3, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldRemoveFirstStudent_whenIsOnlyOneObject() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //When
+        sortedDoublyLinkedList.removeFirst();
+        //Then
+        assertEquals(0, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldRemoveLastStudent_whenIsOnlyOneStudent() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //When
+        sortedDoublyLinkedList.removeLast();
+        //Then
+        assertEquals(0, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldThrowRemovalFromEmptyListException_whenTryRemoveLastStudentFromEmptyList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        //When && then
+        assertThrows(RemovalFromEmptyListException.class, sortedDoublyLinkedList::removeLast);
+    }
+
+    @Test
+    void shouldThrowRemovalFromEmptyListException_whenTryRemoveFirstStudentFromEmptyList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        //When && then
+        assertThrows(RemovalFromEmptyListException.class, sortedDoublyLinkedList::removeFirst);
+    }
+
+    @Test
+    void shouldThrowRemovalFromEmptyListException_whenTryRemoveStudentByIndexFromEmptyList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        //When && then
+        assertThrows(RemovalFromEmptyListException.class, () -> {
+            sortedDoublyLinkedList.removeAt(0);
+        });
+    }
+
+    @Test
+    void shouldThrowIndexOutOfBoundsException_whenTryRemoveStudentByIndexFromEmptyList() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //When && then
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            sortedDoublyLinkedList.removeAt(1);
+        });
+    }
+
+    @Test
+    void shouldRemoveStudentByIndex_whenIsOnlyOneStudent() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        //When
+        sortedDoublyLinkedList.removeAt(0);
+        //Then
+        assertEquals(0, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldRemoveStudentByIndex_whenIsTwoStudents() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianSmith = new Student("Damian", "Smith", 5213);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(damianSmith);
+        //When
+        sortedDoublyLinkedList.removeAt(1);
+        //Then
+        assertEquals(1, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldRemoveStudentByIndex_whenIsManyStudents() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(romanSmith);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        //When
+        sortedDoublyLinkedList.removeAt(2);
+        //Then
+        assertEquals(3, sortedDoublyLinkedList.size());
+    }
+
+    @Test
+    void shouldAddTwoStudentsAndThenReverseList_whenIsReversedAddStudent() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.reverse();
+        //Then
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(patrykSmith);
+        sortedDoublyLinkedList.add(romanSmith);
+        //Then
+        assertEquals(6, sortedDoublyLinkedList.size());
+        assertEquals("Roman", sortedDoublyLinkedList.get(0).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(0).getStudent().surname());
+        assertEquals(3312, sortedDoublyLinkedList.get(0).getStudent().s());
+        assertEquals("Patryk", sortedDoublyLinkedList.get(1).getStudent().name());
+        assertEquals("Smith", sortedDoublyLinkedList.get(1).getStudent().surname());
+        assertEquals(1411, sortedDoublyLinkedList.get(1).getStudent().s());
+        assertEquals("Damian", sortedDoublyLinkedList.get(2).getStudent().name());
+        assertEquals("Nowak", sortedDoublyLinkedList.get(2).getStudent().surname());
+        assertEquals(6142, sortedDoublyLinkedList.get(2).getStudent().s());
+        assertEquals("Rafal", sortedDoublyLinkedList.get(3).getStudent().name());
+        assertEquals("Nawrocki", sortedDoublyLinkedList.get(3).getStudent().surname());
+        assertEquals(2543, sortedDoublyLinkedList.get(3).getStudent().s());
+        assertEquals("Pawel", sortedDoublyLinkedList.get(4).getStudent().name());
+        assertEquals("Kowalski", sortedDoublyLinkedList.get(4).getStudent().surname());
+        assertEquals(2342, sortedDoublyLinkedList.get(4).getStudent().s());
+        assertEquals("Robert", sortedDoublyLinkedList.get(5).getStudent().name());
+        assertEquals("Kowalczyk", sortedDoublyLinkedList.get(5).getStudent().surname());
+        assertEquals(6742, sortedDoublyLinkedList.get(5).getStudent().s());
+    }
+    @Test
+    void shouldDisplayMinIndex_WhenUseSupplyTo() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        MinFinder minFinder = new MinFinder();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(patrykSmith);
+        sortedDoublyLinkedList.add(romanSmith);
+        //When
+        sortedDoublyLinkedList.supplyTo(minFinder);
+        //Then
+        assertEquals(patrykSmith.s(), minFinder.getTrackedStudent().s());
+    }
+
+    @Test
+    void shouldDisplayMaxIndex_WhenUseSupplyTo() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        MaxFinder maxFinder = new MaxFinder();
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(patrykSmith);
+        sortedDoublyLinkedList.add(romanSmith);
+        //When
+        sortedDoublyLinkedList.supplyTo(maxFinder);
+        //Then
+        assertEquals(robertKowalczyk.s(), maxFinder.getTrackedStudent().s());
+    }
+
+    @Test
+    void shouldDisplayIndex_WhenUseSupplyTo() {
+        //Given
+        SortedDoublyLinkedList sortedDoublyLinkedList = new SortedDoublyLinkedList();
+        IndexFinder indexFinder = new IndexFinder(2543);
+        Student pawelKowalski = new Student("Pawel", "Kowalski", 2342);
+        Student damianNowak = new Student("Damian", "Nowak", 6142);
+        Student romanSmith = new Student("Roman", "Smith", 3312);
+        Student robertKowalczyk = new Student("Robert", "Kowalczyk", 6742);
+        Student rafalNawrocki = new Student("Rafal", "Nawrocki", 2543);
+        Student patrykSmith = new Student("Patryk", "Smith", 1411);
+        sortedDoublyLinkedList.add(damianNowak);
+        sortedDoublyLinkedList.add(pawelKowalski);
+        sortedDoublyLinkedList.add(rafalNawrocki);
+        sortedDoublyLinkedList.add(robertKowalczyk);
+        sortedDoublyLinkedList.add(patrykSmith);
+        sortedDoublyLinkedList.add(romanSmith);
+        //When
+        sortedDoublyLinkedList.supplyTo(indexFinder);
+        //Then
+        assertEquals(rafalNawrocki.s(), indexFinder.getTrackedStudent().s());
+    }
+}
